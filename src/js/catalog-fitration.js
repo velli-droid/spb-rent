51a849f12e2914463f103d8fd7f3428ecad1d908
Valeriy <v.boyarinov@pixelplus.ru>
2024-06-20 17:48:58 +0300

    filtration

--- src/js/catalog-fitration.js
+++ src/js/catalog-fitration.js
@@ -1,7 +1,5 @@
 import { CatalogWidget } from "../catalogWidget";
-import { useCatalogStore } from "../store/catalog-store";
 
 export function initCatalogFiltration() {
-  const humans = useCatalogStore((state) => state.humans);
-  const catalog = new CatalogWidget({ humans, is_auth: true });
+  const catalog = new CatalogWidget({ humans: 1, is_auth: true });
 }
